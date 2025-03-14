46457851f4a22ae44a9b3ff66ab3c0058e1f2805
rajan shrestha <stharajan477@gmail.com>
2025-03-14 14:46:58 +0545

    basic project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+function App() {
+  // useState is a hook that allows functional components to have state.
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    if (count < 20) setCount(count + 1);
+  };
+  const decrement = () => {
+    if (count > 0) setCount(count - 1);
+  };
+  return (
+    <>
+      <button onClick={increment}>ADD</button>
+      <h1>Count:{count}</h1>
+      <button onClick={decrement}>Subtract</button>
+    </>
+  );
+}
+export default App;
